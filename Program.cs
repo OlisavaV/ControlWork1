9bef15fff5f34d1860537edcbbd2081fb967d201
OlisavaV <elizavetapr947@gmail.com>
2024-05-07 14:37:24 +0300

    add programm

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+// длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+// Примеры:
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
+
+
+Console.WriteLine("Введите значения массива через пробел ");
+string stroka = Console.ReadLine();
+string[] array = stroka.Split();
+string[] new_array = new string[array.Length];
+
+for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            new_array[i] = array[i];
+        }
+    }
+    
+void PrintArray(string[] chars)
+{
+    Console.Write("[");
+    for (int i = 0; i < chars.Length; i++)
+    {
+        Console.Write($"{chars[i]} ");
+    }
+    Console.Write("]");
+}
+
+PrintArray(new_array);
